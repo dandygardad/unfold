a4689e4a562af3d4659723350d9f5c3e4dd86ba1
dandygardad <dandygardad@gmail.com>
2022-06-23 21:05:46 +0800

    Update: change folder name for saving RMSE json file

--- helper/rmse.py
+++ helper/rmse.py
@@ -5,8 +5,8 @@ import os
 import json
 
 def saveData(name, data):
-    if not os.path.exists(os.getcwd() + '\\rmse'):
+    if not os.path.exists(os.getcwd() + '\\result-rmse'):
         os.makedirs('rmse')
-    with open("./rmse/"+ str(name) +".json", 'w+') as result:
+    with open("./result-rmse/"+ str(name) +".json", 'w+') as result:
         json.dump(data, result)
         print("\nSaved in " + './rmse/' + str(name) + '.json', end="\n")
